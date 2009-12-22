ece2b3a9857bf7efa0efcb4ffd396cbebe03bf79
Whiteknight <wknight8111@gmail.com>
2009-12-22 12:44:44 -0500

    some stuff

--- parrot_utils.sh
+++ parrot_utils.sh
@@ -1,3 +1,6 @@
+# Addons to make life easier developing Parrot
+
+# Uninstall an installed Parrot. Nuke.
 function parrot-uninstall {
     sudo rm -rfv /usr/local/bin/parrot*
     sudo rm -rfv /usr/local/bin/pbc*
@@ -7,18 +10,22 @@ function parrot-uninstall {
     sudo rm -rfv /usr/local/include/parrot*
 }
 
+# If we have ccache, use that because it speeds things up significantly
 WKPARROTUSECCACHE=""
 which ccache &> /dev/null && WKPARROTUSECCACHE="--cc='ccache gcc'"
 
+# If we have flex and bison, set that up. No sense in not using them
 WKPARROTMAINTAINER=""
 which flex &> /dev/null && which bison &> /dev/null && WKPARROTMAINTAINER="--maintainer"
 
-alias pc="perl Configure.pl $PARROTUSECCACHE $PARROTMAINTAINER"
+# Configuration shorthands.
+alias pc="perl Configure.pl $WKPARROTUSECCACHE $WKPARROTMAINTAINER"
 alias pcn="parrot-nqp Configure.nqp"
-
+alias pcs="parrot setup.pir build"
 # What to do here if g++ is not installed? Just let Configure.pl catch it?
-alias pc++="perl Configure.pl $PARROTMAINTAINER --cc=g++ --cxx=g++ --link=g++"
+alias pc++="perl Configure.pl $WKPARROTMAINTAINER --cc=g++ --cxx=g++ --link=g++"
+# TODO: Add configuration shorthands for other compilers too (LLVM?)
 
 # I find 5 is a pretty optimum number, don't need to use NUMTHREADS.
-alias pt="mj TEST_JOBS=5"
+alias pt="make TEST_JOBS=5"
 
