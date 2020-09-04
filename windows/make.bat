f7868b75d40c52ef44845de5eeffcc8d5bcbf1b3
Whiteknight <wknight8111@gmail.com>
2020-09-04 09:45:16 -0400

    Great cleanup of 2020
    Remove all old scripts and files which are no longer useful or necessary
    
    A few small things were kept which might still serve a purpose

--- windows/make.bat
+++
@@ -1,2 +0,0 @@
-:: If we have nmake, use that
-:: If we have mingw32-make from strawberry perl, use that instead
--- windows/up.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if exist .svn goto have_dot_svn
-goto _end
-:have_dot_svn
-svn up
-:_end
-@echo on
