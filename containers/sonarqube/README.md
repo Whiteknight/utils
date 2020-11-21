b42c2bcd0fa35458b073ab0f9f5855498403e4f5
Whiteknight <wknight8111@gmail.com>
2020-11-21 15:10:08 -0500

    Start modernizing my sonarqube process and update readme with more deets

--- containers/sonarqube/README.md
+++ containers/sonarqube/README.md
@@ -1,4 +1,40 @@
-Run ./setup.sh first to make sure the folders and things are created
-docker-compose up -d to start the thing
-connect in browser to localhost:9000 (it takes about 30-60s for the service to be responsive)
+# Setup Sonarqube
+
+## Install OpenCover
+
+```
+dotnet add package --global OpenCover
+```
+
+This will install it to someplace like `~/.nuget/OpenCover/.../tools/OpenCover.Console.exe` Make sure a shortcut to that called "opencover" is located in %PATH%
+
+## Install SonarScanner
+
+```
+dotnet tool install --global dotnet-sonarscanner --version 4.8.0
+```
+
+## Setup the Environment
+
+Run ./setup.sh first to make sure the folders and things are created. Only need to do this the first time
+
+## Run the container
+
+```
+docker-compose up -d
+```
+
+## Setup the Proxy
+
+Make sure nginx is running and configured to proxy `http://sonarqube.local` to `http://localhost:9000`
+
+Update `hosts` file to redirect `sonarqube.local` to `127.0.0.1`
+
+## Browse the Website
+
+Connect in browser to `http://sonarqube.local` (it takes about 30-60s for the service to be responsive)
 login as admin:admin
+
+## Setup Projects
+
+Create projects for everything you want to scan, generate a token for each project, and set a global env variable named `SQ_<projectname>_KEY` with the key value
--- containers/sonarqube/scan-parserobjects.sh
+++ containers/sonarqube/scan-parserobjects.sh
@@ -2,30 +2,29 @@
 # Go to the parserobjects dir, wherever it is
 pg parserobjects
 
-# This key is for a local instance of SonarQube, and needs to be recreated for each new instance
-SONARQUBE_PARSEROBJECTS_KEY=3ce2a9457a631ece9e2e089e027b11837ab3e1fc
-
-# Path to the OpenCover.Console.exe executable
-# TODO: see if we can find a way to make this a relative path for a more repeatable process
-OPENCOVER_EXE=/c/Users/awhitworth/.nuget/packages/opencover/4.7.922/tools/OpenCover.Console.exe
+# PREREQUISITES
+# Make sure sonarscanner is installed
+# Make sure OpenCover.Console.exe is installed, on your path, and named "opencover"
+# Make sure you have a sonarqube project key for ParserObjects set to env var SQ_PARSEROBJECTS_KEY
 
 # Need to install sonarscanner tool in your local for this to work
 # dotnet tool install --global dotnet-sonarscanner --version 4.10.0
 dotnet sonarscanner begin \
     -k:ParserObjects \
-    -d:sonar.login=$SONARQUBE_PARSEROBJECTS_KEY \
+    -d:sonar.host.url="http://sonarqube.local" \
+    -d:sonar.login=$SQ_PARSEROBJECTS_KEY \
     -d:"sonar.cs.opencover.reportsPaths=coverage.xml"
 
 dotnet build ParserObjects.sln
 
-$OPENCOVER_EXE \
+opencover \
     -target:"c:\Program Files\dotnet\dotnet.exe" \
     -targetargs:"test ParserObjects.sln" \
     -output:"coverage.xml" \
     -oldstyle \
     -register:user
 
-dotnet sonarscanner end -d:sonar.login=$SONARQUBE_PARSEROBJECTS_KEY
+dotnet sonarscanner end -d:sonar.login=$SQ_PARSEROBJECTS_KEY
 rm coverage.xml
 
 # Go back to the sonarqube project dir
