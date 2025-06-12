6812e684eb155c15e26dd186c03e00efa32946e2
Andrew Whitworth <awhitworth@halfpenny.com>
2025-06-12 07:27:13 -0400

    pgadmin do not check for updates

--- containers/postgres/run.sh
+++ containers/postgres/run.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
 
 pg_name=postgres
 pgadmin_name=pgadmin
+
+if [ ! -d "/c/projects/utils/containers/postgres/data" ]; then
+    mkdir -p "/c/projects/utils/containers/postgres/data"
+fi
 
 if [[ ! $(docker ps -a --filter="name=$pg_name" | grep -w "$pg_name") ]]; then
     echo "Creating container $pg_name";
@@ -8,7 +13,7 @@ if [[ ! $(docker ps -a --filter="name=$pg_name" | grep -w "$pg_name") ]]; then
         --name $pg_name \
         -e POSTGRES_PASSWORD=strongPassword123! \
         -p 5432:5432 \
-        -v 'C:\\containers\\postgres\\data:/var/lib/postgresql/data' \
+        -v 'C:\\projects\\utils\\containers\\postgres\\data:/var/lib/postgresql/data' \
         postgres;
 else
     echo "Container $pg_name already exists";
@@ -22,6 +27,7 @@ if [[ ! $(docker ps -a --filter="name=$pgadmin_name" | grep -w "$pgadmin_name") 
         --name $pgadmin_name \
         -e PGADMIN_DEFAULT_PASSWORD=strongPassword123! \
         -e PGADMIN_DEFAULT_EMAIL=awhitworth@hc1.com \
+        -e PGADMIN_CONFIG_UPGRADE_CHECK_ENABLED=False \
         -p5050:80 \
         dpage/pgadmin4;
 else
